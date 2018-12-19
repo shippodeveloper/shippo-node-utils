25189f4cd67a3c0928abdcf12dcf3090667e5d99
luuthieu <tronghieu.luu@gmail.com>
2018-12-19 18:01:19 +0700

    Add expression util

---
+++ src/Expression.js
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2018 by Lưu Hiếu <tronghieu.luu@gmail.com>
+ */
+
+let ObjectUtil = require('./ObjectUtil');
+
+class Expression {
+  constructor(condition) {
+    this.condition = condition;
+    this.type = 'AND';
+    this.and = [];
+    this.or = [];
+
+    for (let index in condition) {
+      /*
+       * {
+       *  $or: [
+       *    expression1: value1,
+       *    expression2: value2
+       *  ]
+       * }
+       */
+      if (index === '$or' && Array.isArray(condition.$or)) {
+        for(let ii = 0; ii < condition.$or.length; ++ii) {
+          this.or.push(new Expression(condition.$or[ii]));
+        }
+        this.type = 'OR';
+        break; //$or chi cho phép một phần tử đầu tiên
+      }
+      /*
+       * {
+       *  $and: [
+       *    expression1: value1,
+       *    expression2: value2
+       *  ]
+       * }
+       */
+      else if (index === '$and' && Array.isArray(condition.$and)) {
+        for(let ii = 0; ii < condition.$and.length; ++ii) {
+          this.and.push(new Expression(condition.$and[ii]));
+        }
+        break; //$and giống $or, chúng ta chỉ cho phép một phần tử đầu tiên
+      } else if (Expression._isScalar(condition[index])) { //simple expression
+        /* các expression cơ bản so sánh điều kiện với một giá trị.
+         * exp: {username: 'changiomemuoi', hotline: '0914-819-903', desc: 'Đảm bảo ngon, người nhà ăn suốt'}
+         */
+        this.and.push([
+          index, '$eq', condition[index]
+        ]);
+      } else { //object
+        for (let subIndex in condition[index]) {
+          if (subIndex === '$in' && Array.isArray(condition[index][subIndex])) {
+            /* điều kiện in có thể coi là một điều kiện or, vì vậy cách giải quyết là chuyển nó thành điều kiện or với một Expression */
+            let inCond = Expression._makeOrConditionFromInArray(index, condition[index][subIndex]);
+            this.and.push(new Expression(inCond));
+          } else if (['$neq', '$lt', '$lte', '$gt', '$gte'].indexOf(subIndex) !== -1) {
+            /* các operator được định nghĩa */
+            this.and.push([
+              index, subIndex, condition[index][subIndex]
+            ]);
+          } else {
+            /* Xem ra cấu trúc chỗ này là một điều kiện lồng sâu hơn, vì vậy sử dụng Expression cho phần điều kiện lồng bên trong này */
+            this.and.push(new Expression(condition[index]))
+          }
+        }
+      }
+    }
+
+    /* console.log(this.and) */;
+  }
+
+  test(input) {
+    input = ObjectUtil.flatten(input);
+    if (this.type === 'AND') {
+      return this._testAnd(input);
+    } else if (this.type === 'OR') {
+      return this._testOr(input);
+    }
+
+    throw new TypeError('Expression not valid');
+  }
+
+  /**
+   * Test multiple conditions, return false if one condition false
+   *
+   * @param input
+   * @returns {boolean}
+   * @private
+   */
+  _testAnd(input) {
+    for(let ii = 0; ii < this.and.length; ++ii) {
+      if (!Expression._compare(this.and[ii], input)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Test require one of conditions, return true if one condition true
+   *
+   * @param input
+   * @returns {boolean}
+   * @private
+   */
+  _testOr(input) {
+    for(let ii = 0; ii < this.or.length; ++ii) {
+      if (Expression._compare(this.or[ii], input)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Check is scalar
+   * @param mixVar
+   * @returns {boolean}
+   * @private
+   */
+  static _isScalar(mixVar) {
+    return (/boolean|number|string/).test(typeof mixVar);
+  }
+
+  /**
+   *
+   * @param condition
+   * @param input
+   * @returns {*}
+   * @private
+   */
+  static _compare (condition, input) {
+    if (condition instanceof Expression) {
+      return condition.test(input);
+    } else if (Array.isArray(condition) && condition.length === 3) {
+      if (condition[1] === '$eq' && input[condition[0]] != condition[2]) {
+        return false;
+      }
+      if (condition[1] === '$neq' && input[condition[0]] == condition[2]) {
+        return false;
+      }
+      if (condition[1] === '$gt' && input[condition[0]] <= condition[2]) {
+        return false;
+      }
+      if (condition[1] === '$gte' && input[condition[0]] < condition[2]) {
+        return false;
+      }
+      if (condition[1] === '$lt' && input[condition[0]] >= condition[2]) {
+        return false;
+      }
+      if (condition[1] === '$lte' && input[condition[0]] > condition[2]) {
+        return false;
+      }
+
+      return true;
+    }
+
+    throw new Error('Unknown expression!');
+  }
+
+  static _makeOrConditionFromInArray (index, inCondition) {
+    let cond = [];
+    for (let ii = 0 ; ii < inCondition.length; ++ii) {
+      cond.push({[index]: inCondition[ii]});
+    }
+    return {$or: cond};
+  }
+}
+
+module.exports = Expression;
---
+++ test/expression.test.js
@@ -0,0 +1,269 @@
+/*
+ * Copyright (c) 2018 by Lưu Hiếu <tronghieu.luu@gmail.com>
+ */
+
+const chai = require('chai');
+const Expression = require('./../').Expression;
+
+describe('Test expression:', () => {
+  describe('single condition:', () => {
+    it ('equal $eq', () => {
+      let cond = { 'order.weight': 5000};
+
+      let validInput = {
+        order: {
+          weight: 5000,
+        }
+      };
+
+      let invalidInput = {
+        order: {
+          weight: 4999,
+        }
+      };
+
+      let expression = new Expression(cond);
+      chai.assert.equal(expression.test(validInput), true);
+      chai.assert.equal(expression.test(invalidInput), false)
+    });
+
+    it('not equal $neq', () => {
+      let cond = { 'order.weight': {'$neq': 5000}};
+
+      let invalidInput = {
+        order: {
+          weight: 5000,
+        }
+      };
+
+      let validInput = {
+        order: {
+          weight: 4999,
+        }
+      };
+
+      let expression = new Expression(cond);
+      chai.assert.equal(expression.test(validInput), true);
+      chai.assert.equal(expression.test(invalidInput), false)
+    });
+
+    it('greater than $gt', () => {
+      let cond = {'order.weight': {'$gt': 4999 }};
+      let input = { order: { weight: 5000}};
+      let expression = new Expression(cond);
+      chai.assert.equal(expression.test(input), true);
+      input.order.weight = 50023;
+      chai.assert.equal(expression.test(input), true);
+      input.order.weight = 4999;
+      chai.assert.equal(expression.test(input), false);
+      input.order.weight = 4988;
+      chai.assert.equal(expression.test(input), false);
+    });
+
+    it('greater than or equal $gte', () => {
+      let cond = {'order.weight': {'$gte': 4999 }};
+      let input = { order: { weight: 4999}};
+      let expression = new Expression(cond);
+      chai.assert.equal(expression.test(input), true);
+      input.order.weight = 5000;
+      chai.assert.equal(expression.test(input), true);
+      input.order.weight = 4998;
+      chai.assert.equal(expression.test(input), false);
+      input.order.weight = 4988;
+      chai.assert.equal(expression.test(input), false);
+    });
+
+    it('less than $lt', () => {
+      let cond = {'order.weight': {'$lt': 4999 }};
+      let input = { order: { weight: 4998}};
+      let expression = new Expression(cond);
+      chai.assert.equal(expression.test(input), true);
+      input.order.weight = 4900;
+      chai.assert.equal(expression.test(input), true);
+      input.order.weight = 4999;
+      chai.assert.equal(expression.test(input), false);
+      input.order.weight = 50001;
+      chai.assert.equal(expression.test(input), false);
+    });
+
+    it('less than or equal $lte', () => {
+      let cond = {'order.weight': {'$lte': 4999 }};
+      let input = { order: { weight: 4999}};
+      let expression = new Expression(cond);
+      chai.assert.equal(expression.test(input), true);
+      input.order.weight = 4988;
+      chai.assert.equal(expression.test(input), true);
+      input.order.weight = 5000;
+      chai.assert.equal(expression.test(input), false);
+      input.order.weight = 5900;
+      chai.assert.equal(expression.test(input), false);
+    });
+
+    it('in a collection $in', () => {
+      let cond = {'order.id': {$in: [11,22,33,44,55,66,77,88,99,100]}};
+      let input = { order: { id: 11}};
+      let expression = new Expression(cond);
+      chai.assert.equal(expression.test(input), true);
+      input.order.id = 55;
+      chai.assert.equal(expression.test(input), true);
+      input.order.id = 12;
+      chai.assert.equal(expression.test(input), false);
+      input.order.id = 5900;
+      chai.assert.equal(expression.test(input), false);
+    });
+
+    it('or condition $and', () => {
+      let cond = {
+        $and: [
+          {'order.locationId': {$in: [1,2,3,4,5,6,7,8,9,10]}},
+          {'order.weight': {$lte: 5000}},
+          {'order.package': 'STC'}
+        ]};
+
+      let input = {
+        order: {
+          locationId: 1,
+          weight: 5000,
+          package: 'STC'
+        },
+        merchant: {
+          groupId: 12
+        }
+      };
+
+      let expression = new Expression(cond);
+      chai.assert.equal(expression.test(input), true);
+      input.order.locationId = 9;
+      chai.assert.equal(expression.test(input), true);
+      input.order.weight = 4500;
+      chai.assert.equal(expression.test(input), true);
+      //false
+      input.order.package = 'SCN';
+      chai.assert.equal(expression.test(input), false);
+      input.order.weight = 5500;
+      chai.assert.equal(expression.test(input), false);
+    });
+
+    it('or condition $or', () => {
+      let cond = {
+        $or: [
+          {'order.locationId': {$in: [1,2,3,4,5,6,7,8,9,10]}},
+          {'order.weight': {$lte: 5000}},
+          {'order.package': 'STC'}
+        ]};
+
+      let input = {
+        order: {
+          locationId: 1,
+          weight: 5000,
+          package: 'STC'
+        },
+        merchant: {
+          groupId: 12
+        }
+      };
+
+      let expression = new Expression(cond);
+      chai.assert.equal(expression.test(input), true);
+      input.order.locationId = 12;
+      chai.assert.equal(expression.test(input), true);
+      input.order.weight = 5500;
+      chai.assert.equal(expression.test(input), true);
+      //false
+      input.order.package = 'SCN';
+      chai.assert.equal(expression.test(input), false);
+    });
+  });
+
+  describe('combine conditions:', () => {
+    it('multiple single expression', () => {
+      let cond = {
+        'order.weight': 1234,
+        'order.locationId': 567,
+        'order.value1': 'Ok, im value1, what do u thing about a better name?',
+        'merchant.username': 'tronghieu'
+      };
+
+      let input = {
+        order: {
+          weight: 1234,
+          locationId: 567,
+          value1: 'Ok, im value1, what do u thing about a better name?'
+        },
+        merchant: {
+          username: 'tronghieu'
+        }
+      };
+
+      let expression = new Expression(cond);
+      //true
+      chai.assert.equal(expression.test(input), true);
+      input.order.weight = 12345;
+      chai.assert.equal(expression.test(input), false);
+    });
+
+    it('combine $or expressions', () => {
+      let cond = {
+        'order.package': 'STC',
+        'order.weight': {
+          '$or': [
+            {'order.weight': 1000},
+            {'order.weight': 2000},
+            {'order.weight': 3000},
+          ]
+        },
+        'merchant.username': 'tronghieu'
+      };
+
+      let input = {
+        order: {
+          weight: 1000,
+          locationId: 567,
+          package: 'STC',
+          value1: 'Ok, im value1, what do u thing about a better name?'
+        },
+        merchant: {
+          username: 'tronghieu'
+        }
+      };
+
+      let expression = new Expression(cond);
+      //true
+      chai.assert.equal(expression.test(input), true);
+      input.order.weight = 12345;
+      chai.assert.equal(expression.test(input), false);
+    });
+
+    it('combine $and expression', () => {
+      let cond = {
+        'order.package': 'SCN',
+        'order.weight': {
+          '$and': [
+            {'order.weight': {'$gt': 500 }},
+            {'order.weight': {'$lte': 1500}},
+            //Weight from 500g to 1500g
+          ]
+        },
+        'merchant.username': 'tronghieu'
+      };
+
+      let input = {
+        order: {
+          weight: 1000,
+          locationId: 567,
+          package: 'STC',
+          value1: 'Ok, im value1, what do u thing about a better name?'
+        },
+        merchant: {
+          username: 'tronghieu'
+        }
+      };
+
+      let expression = new Expression(cond);
+      //true
+      chai.assert.equal(expression.test(input), true);
+      input.order.weight = 12345;
+      chai.assert.equal(expression.test(input), false);
+    });
+  });
+});
