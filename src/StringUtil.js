58f6c5ee6eddcea231f01b3c75d942b53e7c61e5
bon <tranvanhoan@velacorp.vn>
2018-12-25 15:21:08 +0700

    add string util

--- src/StringUtil.js
+++ src/StringUtil.js
@@ -13,10 +13,57 @@
  * @returns string
  *
  */
-let normalize = ( string, args ) => {
+let formatWithArray = ( string, args ) => {
     return string.replace( /{(\d+)}/g, ( match, number ) => {
         return typeof args[ number ] !== "undefined" ? args[ number ] : match;
     } );
 };
 
-module.exports = {normalize};
+const signed = 'ăâđêôơưàảãạáằẳẵặắầẩẫậấèẻẽẹéềểễệế'
+    +'ìỉĩịíòỏõọóồổỗộốờởỡợớùủũụúừửữựứỳỷỹỵý'
+    +'ĂÂĐÊÔƠƯÀẢÃẠÁẰẲẴẶẮẦẨẪẬẤÈẺẼẸÉỀỂỄỆẾÌỈĨỊÍ'
+    +'ÒỎÕỌÓỒỔỖỘỐỜỞỠỢỚÙỦŨỤÚỪỬỮỰỨỲỶỸỴÝĐÐ';
+
+const unsigned = 'aadeoouaaaaaaaaaaaaaaaeeeeeeeeee'
+    +'iiiiiooooooooooooooouuuuuuuuuuyyyyy'
+    +'AADEOOUAAAAAAAAAAAAAAAEEEEEEEEEEIIIII'
+    +'OOOOOOOOOOOOOOOUUUUUUUUUUYYYYYDD';
+
+let normalize = (str) => {
+    return trimDoubleSpace(unsign(str.trim())).toLowerCase();
+};
+
+let unsign = (str) => {
+    let result = [];
+    for (let ii = 0; ii < str.length; ii++) {
+        let index = signed.indexOf(str[ii]);
+        if (index > -1) {
+            result.push(unsigned[index]);
+        }
+        else {
+            result.push(str[ii]);
+        }
+    }
+    return result.join('');
+};
+
+let trimDoubleSpace = (str) => {
+    while (str.indexOf('  ') > -1) {
+        str = str.replace('  ', ' ');
+    }
+    return str;
+};
+
+let keepOnlyCharacter = (str, keepSpace = true) => {
+    let result = [];
+    for (let ii = 0; ii <str.length; ii++) {
+        let char = str[ii];
+        if ((char >= 'a' && char <= 'z') || (char >= '0' && char <= '9') || (keepSpace && char === ' ')) {
+            result.push(char);
+        }
+    }
+    return result.join('');
+};
+
+
+module.exports = {formatWithArray, normalize, unsign, trimDoubleSpace, keepOnlyCharacter};
--- test/string.test.js
+++ test/string.test.js
@@ -6,22 +6,71 @@ let chai = require('chai');
 let String = require('./../').StringUtil;
 
 describe('Test string.', () => {
-  it('normalize() happy case', () => {
+  it('formatWithArray() happy case', () => {
     let expect = 'Hello merchant, we are Shippee';
-    let result = String.normalize('Hello {0}, we are {1}', [
+    let result = String.formatWithArray('Hello {0}, we are {1}', [
       'merchant',
       'Shippee'
     ]);
     chai.assert.equal(result, expect);
   });
 
-  it('normalize() over parameters', () => {
+  it('formatWithArray() over parameters', () => {
     let expect = 'Hello merchant, we are Shippee';
-    let result = String.normalize('Hello {0}, we are {1}', [
+    let result = String.formatWithArray('Hello {0}, we are {1}', [
       'merchant',
       'Shippee',
       'Ha ha ha'
     ]);
     chai.assert.equal(result, expect);
+  });
+
+  it('normalize() will converts all of capitalize cases to normal cases', () => {
+    let expect = 'hello world, we are coming';
+    let actual = String.normalize('Hello world, we are coming');
+
+    chai.assert.equal(actual, expect);
+  });
+
+  it('unsign() will converts Vietnamese to English', () => {
+      let expect = 'Hello world, we are den day';
+      let actual = String.unsign('Hello world, we are đến đây');
+
+      chai.assert.equal(actual, expect);
+  });
+
+  it('trimDoubleSpace() will converts double spaces to a space', () => {
+      let expect = 'Hello world, we are coming';
+      let actual = String.trimDoubleSpace('Hello  world,  we  are  coming');
+
+      chai.assert.equal(actual, expect);
+  });
+
+  it('keepOnlyCharacter() without keep space: remove all of non-letter', () => {
+      let expect = 'helloworldwearecoming';
+      let actual = String.keepOnlyCharacter('hello  world,  we  are  coming!', false);
+
+      chai.assert.equal(actual, expect);
+  });
+
+  it('keepOnlyCharacter() with keep space: remove all of non-letter except space', () => {
+      let expect = 'hello world we are coming';
+      let actual = String.keepOnlyCharacter('hello world, we are coming!');
+
+      chai.assert.equal(actual, expect);
+  });
+
+  it('combine keepOnlyCharacter() with normalize() happy case with keep space', () => {
+      let expect = 'hello world we are coming';
+      let actual = String.keepOnlyCharacter(String.normalize('Hello world, we are coming!'));
+
+      chai.assert.equal(actual, expect);
+  });
+
+  it('combine keepOnlyCharacter() with normalize() happy case without keep space', () => {
+      let expect = 'helloworldwearecoming';
+      let actual = String.keepOnlyCharacter(String.normalize('Hello world, we are coming!'), false);
+
+      chai.assert.equal(actual, expect);
   });
 });
