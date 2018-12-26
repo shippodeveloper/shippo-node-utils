22bfbd4c1e0b567118cdf1b954dbfc347611141d
luuthieu <tronghieu.luu@gmail.com>
2018-12-26 17:40:39 +0700

    Add number util

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ let Timer = require('./src/Timer');
 let Logger = require('./src/Logger');
 let Expression = require('./src/Expression');
 let ObjectUtil = require('./src/ObjectUtil');
+let NumberUtil = require('./src/NumberUtil');
 
-module.exports = {StringUtil, Timer, Logger, Expression, ObjectUtil};
+module.exports = {StringUtil, Timer, Logger, Expression, ObjectUtil , NumberUtil};
---
+++ test/number.test.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2018 by Lưu Hiếu <tronghieu.luu@gmail.com>
+ */
+
+let assert = require('chai').assert;
+let NumberUtil = require('./../').NumberUtil;
+
+describe('NumberUtil', () => {
+  describe('true case', () => {
+    it('common usage', () => {
+      assert.equal(NumberUtil.isNumeric(10), true, '10 is number');
+      assert.equal(NumberUtil.isNumeric(10.1), true, '10.1 is number');
+      assert.equal(NumberUtil.isNumeric(-1.1), true, '-1.1 is number');
+      assert.equal(NumberUtil.isNumeric(0), true, '0 is number');
+      assert.equal(NumberUtil.isNumeric("10"), true, '"10" value is number');
+      assert.equal(NumberUtil.isNumeric("10.1"), true, '"10.1" value is number');
+      assert.equal(NumberUtil.isNumeric("-1.1"), true, '"-1.1" value is number');
+      assert.equal(NumberUtil.isNumeric("0"), true, '"0" value is number');
+      assert.equal(NumberUtil.isNumeric("013"), true, '"013" value is number');
+      assert.equal(NumberUtil.isNumeric(parseInt('012')), true, '"013" value is number');
+      assert.equal(NumberUtil.isNumeric(parseFloat('012')), true, '"013" value is number');
+    });
+
+    it('special computer value', () => {
+      assert.equal(NumberUtil.isNumeric(5e3), true, '5e3 is number');
+      assert.equal(NumberUtil.isNumeric("5e3"), true, '"5e3" value is number');
+      assert.equal(NumberUtil.isNumeric("0xff"), true, '"0xff" value is number');
+    })
+  });
+
+  it('false case', () => {
+    assert.equal(NumberUtil.isNumeric(Infinity), false, 'Infinity not is number');
+    assert.equal(NumberUtil.isNumeric(NaN), false, 'NaN not is number');
+    assert.equal(NumberUtil.isNumeric(undefined), false, 'undefined not is number');
+    assert.equal(NumberUtil.isNumeric(null), false, 'null not is number');
+    assert.equal(NumberUtil.isNumeric(''), false, 'empty string not is number');
+    assert.equal(NumberUtil.isNumeric('     '), false, 'string with space not is number');
+    assert.equal(NumberUtil.isNumeric('foo'), false, 'string not is number');
+    assert.equal(NumberUtil.isNumeric([]), false, 'empty array not is number');
+    assert.equal(NumberUtil.isNumeric([1]), false, 'array not is number');
+    assert.equal(NumberUtil.isNumeric({}), false, 'object not is number');
+    assert.equal(NumberUtil.isNumeric(() => {}), false, 'function not is number');
+  });
+});
