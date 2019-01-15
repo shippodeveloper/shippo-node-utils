d986a143071c91ecf7cefb59ef4112bcf436e3ed
bon <tranvanhoan@velacorp.vn>
2019-01-15 10:44:34 +0700

    add randomize

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ let Logger = require('./src/Logger');
 let Expression = require('./src/Expression');
 let ObjectUtil = require('./src/ObjectUtil');
 let NumberUtil = require('./src/NumberUtil');
+let Randomizer = require('./src/Randomizer');
 
-module.exports = {StringUtil, Timer, Logger, Expression, ObjectUtil , NumberUtil};
+module.exports = {StringUtil, Timer, Logger, Expression, ObjectUtil , NumberUtil, Randomizer};
